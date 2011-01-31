6de1d466b5fa272ac6e7e96a07207928f9391805
David Chawei Hsu <chawei.hsu@gmail.com>
2011-01-30 17:09:43 -0800

    first prototype

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,9 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  
+  before_filter :set_counter
+  
+  def set_counter
+    @count = DisablingLog.total_count
+  end
 end
--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,5 @@
 class HomeController < ApplicationController
   def index
-    @count = DisablingLog.total_count
+    
   end
 end

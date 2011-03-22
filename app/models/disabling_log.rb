ec9f4e285226a4f1fe5423da7bfe39eea6c09a9a
David Chawei Hsu <chawei.hsu@gmail.com>
2011-03-22 12:43:38 -0700

    determine ip location

--- app/models/disabling_log.rb
+++ app/models/disabling_log.rb
@@ -3,4 +3,11 @@ class DisablingLog < ActiveRecord::Base
   def self.total_count
     DisablingLog.sum :button_count
   end
+  
+  def location
+    return nil if request_ip.blank?
+    
+    geo = Autometal::Geoip.new(request_ip)
+    return "#{geo.city}, #{geo.country}"
+  end
 end
